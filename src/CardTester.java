9a1af7f110b3a48d050cf4c4fb4c936b03a7c4cb
Kyle Steinsvaag <stonebay.kyle@gmail.com>
2020-03-23 15:14:51 -0600

    Completes activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -10,6 +10,14 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("Three", "Spades", 3);
+		Card card2 = new Card("Four", "Diamonds", 4);
+		Card card3 = new Card("Three", "Spades", 3);
+
+		System.out.println(card1);
+		System.out.println("Card 1 matches card 2? " + card1.matches(card2));
+		System.out.println("Card 1 matches card 3? " + card1.matches(card3));
+		System.out.println("Card 2 matches card 3? " + card2.matches(card3));
+
 	}
 }
